00c8ff76e98389968f07a3b58da51cc6af78acf9
Father Chrysostomos <sprout@cpan.org>
2012-06-30 22:29:28 -0700

    padcv op type

--- opcode.h
+++ opcode.h
@@ -522,6 +522,7 @@ EXTCONST char* const PL_op_name[] = {
 	"coreargs",
 	"runcv",
 	"fc",
+	"padcv",
 };
 #endif
 
@@ -903,6 +904,7 @@ EXTCONST char* const PL_op_desc[] = {
 	"CORE:: subroutine",
 	"__SUB__",
 	"fc",
+	"private subroutine",
 };
 #endif
 
@@ -1298,6 +1300,7 @@ EXT Perl_ppaddr_t PL_ppaddr[] /* or perlvars.h */
 	Perl_pp_coreargs,
 	Perl_pp_runcv,
 	Perl_pp_fc,
+	Perl_pp_padcv,
 }
 #endif
 #ifdef PERL_PPADDR_INITED
@@ -1690,6 +1693,7 @@ EXT Perl_check_t PL_check[] /* or perlvars.h */
 	Perl_ck_null,		/* coreargs */
 	Perl_ck_null,		/* runcv */
 	Perl_ck_fun,		/* fc */
+	Perl_ck_null,		/* padcv */
 }
 #endif
 #ifdef PERL_CHECK_INITED
@@ -2076,6 +2080,7 @@ EXTCONST U32 PL_opargs[] = {
 	0x00000600,	/* coreargs */
 	0x00000004,	/* runcv */
 	0x00009b8e,	/* fc */
+	0x00000040,	/* padcv */
 };
 #endif
 
